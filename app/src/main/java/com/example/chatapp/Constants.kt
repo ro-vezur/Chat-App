e256953cb213996b06ffe5572e78a6e34d05adf7
ro-vezur <authenttesting@gmail.com>
2025-03-01 15:54:24 +0200

    feat: constants

--- app/src/main/java/com/example/chatapp/Constants.kt
+++ app/src/main/java/com/example/chatapp/Constants.kt
@@ -2,7 +2,10 @@ package com.example.chatapp
 
 
 const val webClientId = BuildConfig.webClientID
-
 const val USERS_DB_COLLECTION = "users"
-
 const val FCM_PREFERENCES = "FCM_PREFERENCES"
+
+const val MAX_LOGIN_ATTEMPTS = 3
+const val AWAIT_TIME_UNTIL_UNBLOCK = 1
+
+const val ACCURATE_DATE_FORMAT = "dd/MM/yyyy hh:mm"
