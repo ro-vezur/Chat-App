9fcd790c77666c802e1fcd0feef773f5e6b320fe
ro-vezur <authenttesting@gmail.com>
2025-03-27 23:58:21 +0200

    feat: other chat usecases

--- app/src/main/java/com/example/chatapp/model/db/messagesDbUseCases/posts/AddMessageUseCase.kt
+++ app/src/main/java/com/example/chatapp/model/db/messagesDbUseCases/posts/AddMessageUseCase.kt
@@ -3,29 +3,27 @@ package com.example.chatapp.model.db.messagesDbUseCases.posts
 import com.example.chatapp.CHATS_DB_COLLECTION
 import com.example.chatapp.Dtos.chat.Chat
 import com.example.chatapp.Dtos.chat.Message
-import com.example.chatapp.MESSAGES_DB_COLLECTION
+import com.example.chatapp.MESSAGES_DB
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
 class AddMessageUseCase @Inject constructor(
-    private val db: FirebaseFirestore
+    private val fireStore: FirebaseFirestore,
+    private val databaseReference: DatabaseReference,
 ) {
-    private val chatsDb = db.collection(CHATS_DB_COLLECTION)
-    private val messagesDb = db.collection(MESSAGES_DB_COLLECTION)
+    private val chatsDb = fireStore.collection(CHATS_DB_COLLECTION)
 
     suspend operator fun invoke(message: Message) {
         try {
-            db.runTransaction { transaction ->
+            fireStore.runTransaction { transaction ->
                 val chatDocumentRef = chatsDb.document(message.chatId)
                 val chat = transaction[chatDocumentRef].toObject(Chat::class.java)
 
                 chat?.let {
-                    val messagesIDs = chat.messages
-                    messagesIDs.add(0,message.id)
-                    messagesDb.document(message.id).set(message)
-
-                    transaction.update(chatDocumentRef,"messages",messagesIDs)
+                    databaseReference.child(MESSAGES_DB).child(message.chatId).child(message.id).setValue(message)
+                    transaction.update(chatDocumentRef,"lastMessageId",message.id)
                 }
             }.await()
         } catch (e: Exception) {
---
+++ app/src/main/java/com/example/chatapp/model/db/messagesDbUseCases/posts/SetMessagesReadStatusUseCase.kt
@@ -0,0 +1,48 @@
+package com.example.chatapp.model.db.messagesDbUseCases.posts
+
+import android.util.Log
+import com.example.chatapp.Dtos.chat.Message
+import com.example.chatapp.MESSAGES_DB
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.MutableData
+import com.google.firebase.database.Transaction
+import javax.inject.Inject
+
+class SetMessagesReadStatusUseCase @Inject constructor(
+    private val db: DatabaseReference,
+) {
+    operator fun invoke(messagesReadList: List<Message>, chatId: String,userId: String,onSuccess: () -> Unit) {
+        try {
+            messagesReadList.forEach { message ->
+                val messageRef = db.child(MESSAGES_DB).child(chatId).child(message.id)
+                messageRef.runTransaction(
+                    object : Transaction.Handler {
+                        override fun doTransaction(currentData: MutableData): Transaction.Result {
+                            val seenBy = message.seenBy
+                            if(!seenBy.contains(userId)) {
+                                seenBy.add(userId)
+                            }
+                            currentData.value = message
+
+                            return Transaction.success(currentData)
+                        }
+
+                        override fun onComplete(
+                            error: DatabaseError?,
+                            committed: Boolean,
+                            currentData: DataSnapshot?,
+                        ) {}
+                    }
+                )
+            }
+
+            onSuccess()
+        } catch (e: Exception) {
+            Log.e("ERROR TO UPDATE LAST READS",e.message.toString())
+            e.printStackTrace()
+        }
+
+    }
+}
